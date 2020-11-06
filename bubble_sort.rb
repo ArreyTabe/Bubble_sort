77e46e94bf53ac341b3eaeda79cdb95f1f22b0bb
Arrey Tabe <arrey2391@gmail.com>
2020-11-06 15:03:09 +0100

    swaps and sorts array enteries

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -10,62 +10,28 @@
 
 def bubble_sort_by(arr)
     n = arr.size
-    if n <= 1
-        return arr
-    elsif n > 1 
-        (n-1).times {|i|
-           if arr[i] <= arr[i+1] 
-            return arr
-           else
-            temp = 0
-            arr[i] = temp
-            arr[i] = arr[i+1]
-            arr[i+1] = temp
-            
-            print arr
-
-           end 
-        }
+    if n > 1
+    arr_sorted = true
+    while arr_sorted do
+        arr_sorted = false
+        temp = 0
+    (n-1).times do |i|
+      
+        if arr[i] > arr[i+1] 
+      
+         temp = arr[i]
+         arr[i] = arr[i+1]
+         arr[i+1] = temp
+         arr_sorted = true
+        end
     end
-end
-     bubble_sort_by([3, 7, 4, 5, 9])
-    
-#         # arr[i] = arr[0]
-#         temp = 0
-
-       
-#            temp =  arr[i]
-#             arr[i+1] = arr[i]
-#             temp = arr[i+1]
-#             sorted_arr.push(temp)
-#             i += 1
-        
-#         return sorted_arr
-
-#     end
-# end
-
-
-#    
-
-
-
-
-
-
-#     print arr
-# arr.each_with_index { |arr0, arr1| 
   
-#     if arr[0] <= arr[1]
-#   return arr
-#     elsif arr[0] > arr[1]
-#      temp =   arr[0] 
-#      arr[1] = arr[0] 
-#         temp = arr[1]
-#         return arr
-#     end
-    
+end
+return arr
+else  
+    # return arr
+    print "Nothing to sort "
+end
+end
    
-# }
-# end
- 
+print bubble_sort_by([7])
