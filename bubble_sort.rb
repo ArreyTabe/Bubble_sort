ef8f219f3e50a215aece9ce0ce429f5c364746ad
Arrey Tabe <arrey2391@gmail.com>
2020-11-07 09:46:09 +0100

    adds bubble_sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,10 +1,12 @@
-def bubble_sort_by(arr)
+# frozen_string_literal: true
+
+def bubble_sort(arr)
   n = arr.size
   return unless n > 1
 
-  arr_sorted = true
-  while arr_sorted
-    arr_sorted = false
+  sorted_arr = true
+  while sorted_arr
+    sorted_arr = false
     temp = 0
     (n - 1).times do |i|
       next unless arr[i] > arr[i + 1]
@@ -12,9 +14,21 @@ def bubble_sort_by(arr)
       temp = arr[i]
       arr[i] = arr[i + 1]
       arr[i + 1] = temp
-      arr_sorted = true
+      sorted_arr = true
     end
   end
   arr
 end
-print bubble_sort_by([7, 5, 56, 87])
+print bubble_sort([1, 0, -1, -1, -2, -4, 6])
+def bubble_sort_by(arr)
+  arr.each_with_index do |a, b|
+    arr.each_with_index do |r, l|
+      arr[b], arr[l] = arr[l], arr[b] if yield(a, r).positive?
+    end
+  end
+  print arr
+end
+
+bubble_sort_by(%w[hi hello hey]) do |left, right|
+  right.length - left.length
+end
