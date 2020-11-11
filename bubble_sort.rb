d8b90fc2b7fea928d7ea31183df689b1c417646f
Arrey Tabe <arrey2391@gmail.com>
2020-11-11 08:37:11 +0100

    modifies bubble_sort_by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -15,18 +15,39 @@ def bubble_sort(arr)
       sorted_arr = true
     end
   end
-  arr
+   arr
 end
 print bubble_sort([1, 0, -1, -1, -2, -4, 6])
+puts 
+
 def bubble_sort_by(arr)
-  arr.each_with_index do |a, b|
-    arr.each_with_index do |r, l|
-      arr[b], arr[l] = arr[l], arr[b] if yield(a, r).positive?
+
+  n = arr.size
+  return unless n > 1
+
+  sorted_arr = true
+  while sorted_arr
+    sorted_arr = false
+    temp = 0
+    (n - 1).times do |i|
+      next unless yield(arr[i+1], arr[i]).positive?
+
+      temp = arr[i]
+      arr[i] = arr[i + 1]
+      arr[i + 1] = temp
+      sorted_arr = true
     end
   end
   print arr
+
+  # arr.each_with_index do |a, b|
+  #   arr.each_with_index do |r, l|
+  #     arr[b], arr[l] = arr[l], arr[b] if yield(a, r).positive?
+  #   end
+  # end
+  # print arr
 end
 
-bubble_sort_by(%w[hi hello hey]) do |left, right|
+ bubble_sort_by(%w[hi hello hey]) do |left, right|
   right.length - left.length
 end
